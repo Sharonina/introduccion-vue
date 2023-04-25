7ba32a60d073c5a5d811a363efe48743503636a1
Sharon Arana <shar_mdza@hotmail.com>
2023-04-25 15:32:29 -0600

    Hello world

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- <script>
+data() {
+  return {
+
+  }
+},
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>{{ text }}</div>
 </template>
 
 <style scoped>
@@ -28,3 +24,4 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+ -->
